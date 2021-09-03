9787103c133a6c68b7b1e08526ac0484a8177b1f
Aryan Jyotirmay <aryantriestocode@gmail.com>
2021-09-03 23:29:52 +0530

    Update Navigator.kt

--- Application Source code/app/src/main/java/com/example/films/Navigator.kt
+++ Application Source code/app/src/main/java/com/example/films/Navigator.kt
@@ -5,8 +5,8 @@ import android.content.Intent
 import android.widget.Toast
 
 object Navigator {
-    fun zoomImg(context: Context,posPath: String) {
-        if (posPath!=null) {
+    fun zoomImg(context: Context, posPath: String) {
+        if (posPath != null) {
             val i: Intent = Intent(context, enlarge::class.java)
             i.putExtra("imgurl", posPath)
             context.startActivity(i)
@@ -15,7 +15,7 @@ object Navigator {
         }
     }
 
-    fun similarMovies(context: Context,id: String,name: String,themeImage: String) {
+    fun similarMovies(context: Context, id: String, name: String, themeImage: String) {
         val intent = Intent(context, SimilarMoviesActivity::class.java)
         intent.putExtra("id", id)
         intent.putExtra("title", name)
@@ -23,14 +23,14 @@ object Navigator {
         context.startActivity(intent)
     }
 
-    fun seeReviews(context: Context,id: String,name: String) {
+    fun seeReviews(context: Context, id: String, name: String) {
         val reviewIntent = Intent(context, ImageCollections::class.java)
         reviewIntent.putExtra("id", id)
         reviewIntent.putExtra("title", name)
         context.startActivity(reviewIntent)
     }
 
-    fun searchHandler(context: Context,searchTerm: String) {
+    fun searchHandler(context: Context, searchTerm: String) {
         if (searchTerm.count() != 0) {
             val i = Intent(context, SearchActivity::class.java)
             i.putExtra("search", searchTerm)
@@ -38,7 +38,7 @@ object Navigator {
         }
     }
 
-    fun shareButtonA(context: Context,posActor: String,nameShare: String) {
+    fun shareButtonA(context: Context, posActor: String, nameShare: String) {
         val share = Intent(Intent.ACTION_SEND)
         share.type = "text/plain"
         share.putExtra(
@@ -54,7 +54,7 @@ object Navigator {
         }
     }
 
-    fun shareButton(context: Context,name: String,posPath: String) {
+    fun shareButton(context: Context, name: String, posPath: String) {
         val sh = Intent(Intent.ACTION_SEND)
         sh.type = "text/plain"
         sh.putExtra(
