234d6ba14eb4d933e8ee9bfddb113a36adc168c7
Aryan Jyotirmay <aryantriestocode@gmail.com>
2021-09-04 19:42:23 +0530

    update/location-access

--- Application Source code/app/src/main/java/com/example/films/MapsActivity.kt
+++ Application Source code/app/src/main/java/com/example/films/MapsActivity.kt
@@ -1,8 +1,15 @@
 package com.example.films
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.films.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,34 +21,50 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private  var lat: Double = 0.0
+    private  var lng: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        binding.getLocation.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+                        if (location!=null) {
+                         lat = location.latitude
+                         lng = location.longitude
+                        } else {
+                            Toast.makeText(this,"Location: Off",Toast.LENGTH_SHORT).show()
+                            requestPermissions(Array(1) { Manifest.permission.ACCESS_FINE_LOCATION }, 111)
+                        }
+                    }
+
+                } else {
+                    requestPermissions(Array(1) { Manifest.permission.ACCESS_FINE_LOCATION }, 111)
+
+                }
+            }
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+        }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(28.6, 77.2)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in India"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val sydney = LatLng(lat, lng)
+        mMap.addMarker(MarkerOptions().position(sydney))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18F),2000,null)
     }
+
 }
