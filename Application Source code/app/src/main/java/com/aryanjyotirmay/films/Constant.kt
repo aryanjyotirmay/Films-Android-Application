148719f486dadea55ced6cb4a10b2e73d7ea47a8
Aryan Jyotirmay <aryantriestocode@gmail.com>
2021-09-17 21:08:10 +0530

    api keys removed

--- Application Source code/app/src/main/java/com/aryanjyotirmay/films/Constant.kt
+++ Application Source code/app/src/main/java/com/aryanjyotirmay/films/Constant.kt
@@ -2,23 +2,23 @@ package com.aryanjyotirmay.films
 
 object Constant {
 
-    const val apiKey = "6a56f08dcd3c96db9bf09fde79d32bcc"
+    const val apiKey = "Please-put-your-api-key"
 
-    const val apiNewsKey = "0d1b2c7f2adb41f68f945eaedf20959f"
-    const val apiNewsKey2 = "7f16f1263926499db14dda4f3d1b380f"
-    const val apiNewsKey3 = "914e165eb1184a418e639e665119d01b"
+    const val apiNewsKey = "Please-put-your-api-key"
+    const val apiNewsKey2 = "Please-put-your-api-key"
+    const val apiNewsKey3 = "Please-put-your-api-key"
 
     fun getRestAPIKey(): String {
-        return "d07b13459538cb806d6e8411afa3612b"
+        return "Please-put-your-api-key"
     }
 
     fun getAtlasGrantType(): String {
-        return "client_credentials"
+        return "Please-put-your-api-key"
     }
     fun getAtlasClientId(): String {
-        return "33OkryzDZsLlv97wC3Lxbypzup5w8-z_pkyR-kczJcx8p_skMrSQ4XmL5sBY-wRPxQGFiOTxb8XIx0W6OxKZYw=="
+        return "Please-put-your-api-key"
     }
     fun getAtlasClientSecret(): String {
-        return "lrFxI-iSEg_cu2-EnvSvSpK0n__EkynXbT5OrdyQFpBaImMfK-_5aCHeC-awcb6vedbmNwpX0OHTDVbsrhWQA573hg3bIhJm"
+        return "Please-put-your-api-key"
     }
 }
